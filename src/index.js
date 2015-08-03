5f3c2e2f779b3104b0c699eea821849261db048f
cw-ozaki <ozaki@chatwork.co.jp>
2015-08-03 12:15:06 +0900

    fixes: remove enviroment value

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ var client = require('./libs/client');
  * @param {Object} context lambda context
  */
 exports.handler = function(event, context) {
-  var token = config['token'] || process.env.CHATWORK_API_TOKEN;
+  var token = config['token'];
     if (!token) {
       throw new Error("Error: `token` has not been set in the configuration.");
     }
   client.init({token: token});
 
   var promises = event.Records.map(function(record) {
-    var compiled = _.template(config['template'] || process.env.CHATWORK_MESSAGE_TEMPLATE || '');
+    var compiled = _.template(config['template'] || '');
     var messageObj = {};
     if (record.Sns.Message) {
       try {
@@ -27,7 +27,7 @@ exports.handler = function(event, context) {
       }
     }
 
-    var roomId = config['roomId'] || process.env.CHATWORK_SEND_ROOM_ID;
+    var roomId = config['roomId'];
     if (!roomId) {
       throw new Error("Error: `roomId` has not been set in the configuration.");
     }
